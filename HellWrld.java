0e34565e04333b48b32e98b77799d23a811ab880
Pooja <bhosalepooja861@gmail.com>
2023-02-21 08:36:53 +0530

    commiting Hello World changes Code in java

--- HellWrld.java
+++ HellWrld.java
@@ -5,7 +5,7 @@ public class HellWrld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println(" Hello World !!!!!!");
+		System.out.println(" Hello World  with Maven project!.!.!.!.!.!");
 	}
 
 }
