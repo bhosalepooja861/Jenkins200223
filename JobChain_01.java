42e0042474cc2df46b81288645e43520088fd9c4
Pooja <bhosalepooja861@gmail.com>
2023-02-23 23:05:33 +0530

    Adding Job Chaining Code

---
+++ JobChain_01.java
@@ -0,0 +1,11 @@
+//package firstFile;
+
+public class JobChain_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" This is PARENT Job Chaining Code!.!.!");
+	}
+
+}
---
+++ JobChain_02.java
@@ -0,0 +1,11 @@
+//package firstFile;
+
+public class JobChain_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" This is CHILD Job Chaining Code!.!.!.!.!.!");
+	}
+
+}
