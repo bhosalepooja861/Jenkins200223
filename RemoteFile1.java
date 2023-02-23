572f35be58c4724a80e962bd313c7650d6eb16ed
Pooja <bhosalepooja861@gmail.com>
2023-02-23 07:42:21 +0530

    commiting RemoteFile1 code

---
+++ RemoteFile1.java
@@ -0,0 +1,11 @@
+//package firstFile;
+
+public class RemoteFile1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" This my second remote file!.!.!.!.!.!");
+	}
+
+}
