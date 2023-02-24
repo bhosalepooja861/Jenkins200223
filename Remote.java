3676cbdb4c113b7f3d2aecaff1edce1d14e413bf
Pooja <bhosalepooja861@gmail.com>
2023-02-24 18:48:29 +0530

    adding remote file

---
+++ Remote.java
@@ -0,0 +1,11 @@
+//package firstFile;
+
+public class Remote {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" This my first remote file code!.!.!.!.!.!");
+	}
+
+}
